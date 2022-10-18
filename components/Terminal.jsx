23bb4f92d51550234123ff9144bf1f31f65b7176
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-18 19:01:01 +0530

    Add: Github icon to the Terminal

--- components/Terminal.jsx
+++ components/Terminal.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { React, ReactElement } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Terminal = () => {
     return (
         <div className='m-0 p-0 box-border h-[100vh] flex items-center justify-center'>
-            <div className='container'>
+            <div className='container flex flex-col w-full max-w-[800px] h-[500px] overflow-hidden rounded-lg border-gray-400 m-5 cursor-text text-center'>
                 <div className="menu">
                     <div className="buttons-flex">
                         <div className="button red"></div>
@@ -11,16 +13,12 @@ const Terminal = () => {
                         <div className="button green"></div>
                     </div>
                     <div className="title">
-                        <p>Terminal</p>
+                        <a href="https://github.com/ShlokBharadwaj" target="_blank"><h1>
+                            <FontAwesomeIcon icon={faGithub} className="pr-2"/>
+                            github.com/ShlokBharadwaj</h1></a>
                     </div>
                 </div>
-                <div className="terminal">
-                    <div className="terminal-header">
-                        <div className="terminal-header-button red"></div>
-                        <div className="terminal-header-button yellow"></div>
-                        <div className="terminal-header-button green"></div>
-                    </div>
-                </div>
+                <div id="app"></div>
             </div>
         </div>
     )
