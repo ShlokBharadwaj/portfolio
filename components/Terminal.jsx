c8214c49748a6d25ac08440e1dad0df70fc96479
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-18 19:38:19 +0530

    Add: Color, Height to main terminal input area

--- components/Terminal.jsx
+++ components/Terminal.jsx
@@ -7,7 +7,7 @@ const Terminal = () => {
         <div className='m-0 p-0 box-border h-[100vh] flex items-center justify-center'>
             <div className='container flex flex-col w-full max-w-[800px] h-[500px] overflow-hidden rounded-lg border-gray-400 m-5 cursor-text'>
                 <div className="menu flex flex-row items-center text-center w-full h-6 bg-[#424040] py-0 px-2 cursor-default">
-                    <div className="buttons-flex absolute flex flex-row">
+                    <div className="buttons-flex absolute flex flex-row ">
                         <div className="button red bg-[#f5544d] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
                         <div className="button yellow bg-[#fabd2f] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
                         <div className="button green bg-[#47d043] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
@@ -18,7 +18,7 @@ const Terminal = () => {
                             github.com/ShlokBharadwaj</h1></a>
                     </div>
                 </div>
-                <div id="app"></div>
+                <div id="app" className='flex flex-col bg-[#282a35] w-full h-full p-2 overflow-auto'></div>
             </div>
         </div>
     )
