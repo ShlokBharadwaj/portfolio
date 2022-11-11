04f65c3928010b5c5ffe1dfa064098b8c0039db1
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-11-11 16:14:50 +0530

    Update: File for Terminal and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Terminal from "./components/Terminal";
 
 function App() {
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Terminal />
+    </>
   );
 }
 
