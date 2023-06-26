eda822ab33d832e791fc080819a69a3541b1a569
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-06-26 20:37:54 +0530

    Update: Made GNU bash, version 5.2.15(1)-release (x86_64-pc-msys) These shell commands are defined internally.  Type `help' to see this list. Type `help name' to find out more about the function `name'. Use `info bash' to find out more about the shell in general. Use `man -k' or `info' to find out more about commands not in this list.
    A star (*) next to a name means that the command is disabled.
    
     job_spec [&]                            history [-c] [-d offset] [n] or hist>
     (( expression ))                        if COMMANDS; then COMMANDS; [ elif C>
     . filename [arguments]                  jobs [-lnprs] [jobspec ...] or jobs >
     :                                       kill [-s sigspec | -n signum | -sigs>
     [ arg... ]                              let arg [arg ...]
     [[ expression ]]                        local [option] name[=value] ...
     alias [-p] [name[=value] ... ]          logout [n]
     bg [job_spec ...]                       mapfile [-d delim] [-n count] [-O or>
     bind [-lpsvPSVX] [-m keymap] [-f file>  popd [-n] [+N | -N]
     break [n]                               printf [-v var] format [arguments]
     builtin [shell-builtin [arg ...]]       pushd [-n] [+N | -N | dir]
     caller [expr]                           pwd [-LPW]
     case WORD in [PATTERN [| PATTERN]...)>  read [-ers] [-a array] [-d delim] [->
     cd [-L|[-P [-e]] [-@]] [dir]            readarray [-d delim] [-n count] [-O >
     command [-pVv] command [arg ...]        readonly [-aAf] [name[=value] ...] o>
     compgen [-abcdefgjksuv] [-o option] [>  return [n]
     complete [-abcdefgjksuv] [-pr] [-DEI]>  select NAME [in WORDS ... ;] do COMM>
     compopt [-o|+o option] [-DEI] [name .>  set [-abefhkmnptuvxBCEHPT] [-o optio>
     continue [n]                            shift [n]
     coproc [NAME] command [redirections]    shopt [-pqsu] [-o] [optname ...]
     declare [-aAfFgiIlnrtux] [name[=value>  source filename [arguments]
     dirs [-clpv] [+N] [-N]                  suspend [-f]
     disown [-h] [-ar] [jobspec ... | pid >  test [expr]
     echo [-neE] [arg ...]                   time [-p] pipeline
     enable [-a] [-dnps] [-f filename] [na>  times
     eval [arg ...]                          trap [-lp] [[arg] signal_spec ...]
     exec [-cl] [-a name] [command [argume>  true
     exit [n]                                type [-afptP] name [name ...]
     export [-fn] [name[=value] ...] or ex>  typeset [-aAfFgiIlnrtux] name[=value>
     false                                   ulimit [-SHabcdefiklmnpqrstuvxPRT] [>
     fc [-e ename] [-lnr] [first] [last] o>  umask [-p] [-S] [mode]
     fg [job_spec]                           unalias [-a] name [name ...]
     for NAME [in WORDS ... ] ; do COMMAND>  unset [-f] [-v] [-n] [name ...]
     for (( exp1; exp2; exp3 )); do COMMAN>  until COMMANDS; do COMMANDS-2; done
     function name { COMMANDS ; } or name >  variables - Names and meanings of so>
     getopts optstring name [arg ...]        wait [-fn] [-p var] [id ...]
     hash [-lr] [-p pathname] [-dt] [name >  while COMMANDS; do COMMANDS-2; done
     help [-dms] [pattern ...]               { COMMANDS ; } command responsive for smaller screens

--- src/components/commands/Help.jsx
+++ src/components/commands/Help.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 
 const Help = () => {
+  const commands = [
+    { cmd: "help", desc: "check available commands.", tab: 9 },
+    { cmd: "about", desc: "about Shlok Bharadwaj.", tab: 8 },
+    { cmd: "clear", desc: "clear the terminal.", tab: 8 },
+    { cmd: "echo", desc: "print out anything.", tab: 9 },
+    { cmd: "education", desc: "my educational background.", tab: 4 },
+    { cmd: "email", desc: "send me an email.", tab: 8 },
+    { cmd: "gui", desc: "go to the GUI version of my portfolio.", tab: 10 },
+    { cmd: "history", desc: "view list of previous commands.", tab: 6 },
+    { cmd: "projects", desc: "view projects that I've done over the years.", tab: 5 },
+    { cmd: "socials", desc: "check out my social accounts.", tab: 6 },
+    { cmd: "welcome", desc: "display hero section.", tab: 6 },
+    { cmd: "whoami", desc: "about current user.", tab: 7 },
+  ];
 
-    const commands = [
-        { cmd: "help", desc: "check available commands.", tab: 9 },
-        { cmd: "about", desc: "about Shlok Bharadwaj.", tab: 8 },
-        { cmd: "clear", desc: "clear the terminal.", tab: 8 },
-        { cmd: "echo", desc: "print out anything.", tab: 9 },
-        { cmd: "education", desc: "my educational background.", tab: 4 },
-        { cmd: "email", desc: "send me an email.", tab: 8 },
-        { cmd: "gui", desc: "go to the GUI version of my portfolio.", tab: 10 },
-        { cmd: "history", desc: "view list of previous commands.", tab: 6 },
-        { cmd: "projects", desc: "view projects that I've done over the years.", tab: 5 },
-        { cmd: "socials", desc: "check out my social accounts.", tab: 6 },
-        { cmd: "welcome", desc: "display hero section.", tab: 6 },
-        { cmd: "whoami", desc: "about current user.", tab: 7 },
-    ];
-
-    return (
-        <div>
-            {commands.map((command, index) => (
-                <div key={index}>
-                    <span className="text-green-400">{command.cmd}</span>
-                    <span className="text-white">{Array(command.tab).fill("\u00A0").join("")}</span>
-                    <span className="text-gray-400">{command.desc}</span>
-                </div>
-            ))}
+  return (
+    <div>
+      {commands.map((command, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-between"
+        >
+          <span className="text-green-400">{command.cmd}</span>
+          <span className="text-white">{Array(command.tab).fill("\u00A0").join("")}</span>
+          <span className="text-gray-400 whitespace-nowrap">{command.desc}</span>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Help;
