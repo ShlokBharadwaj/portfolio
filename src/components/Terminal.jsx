ebc291e85d313f1f7e4ae5993e8c0452ec3b7db8
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-06-26 13:36:40 +0530

    Add: Terminal minimizes and expands on red,green button click

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -11,6 +11,7 @@ const Terminal = () => {
 
     const [showWelcome, setShowWelcome] = useState(false);
     const [terminalClosed, setTerminalClosed] = useState(false);
+    const [terminalMinimized, setTerminalMinimized] = useState(false);
 
     useEffect(() => {
         const delayToShowWelcome = async () => {
@@ -20,8 +21,18 @@ const Terminal = () => {
         delayToShowWelcome();
     }, []);
 
+    const openTerminal = () => {
+        setTerminalClosed(false);
+        setTerminalMinimized(false);
+    };
+
+    const minimizeTerminal = () => {
+        setTerminalMinimized(prevState => !prevState);
+    };
+
     const closeTerminal = () => {
         setTerminalClosed(true);
+        setTerminalMinimized(false);
     };
 
     return (
@@ -30,8 +41,8 @@ const Terminal = () => {
                 <div className="menu flex flex-row items-center text-center w-full h-6 bg-[#424040] py-0 px-2 cursor-default">
                     <div className="buttons-flex absolute flex flex-row">
                         <div className="button red bg-[#f5544d] w-3 h-3 rounded-full mr-2 cursor-pointer" onClick={closeTerminal}></div>
-                        <div className="button yellow bg-[#fabd2f] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
-                        <div className="button green bg-[#47d043] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
+                        <div className="button yellow bg-[#fabd2f] w-3 h-3 rounded-full mr-2 cursor-pointer" onClick={minimizeTerminal}></div>
+                        <div className="button green bg-[#47d043] w-3 h-3 rounded-full mr-2 cursor-pointer" onClick={openTerminal}></div>
                     </div>
                     <div className="title flex-1 text-center text-slate-200">
                         <a href="https://github.com/ShlokBharadwaj/portfolio" target="_blank">
@@ -45,7 +56,7 @@ const Terminal = () => {
                         </h1>
                     </div>
                 </div>
-                <div className={`app flex flex-col bg-[#282a35] w-full h-full p-2 overflow-auto text-white relative ${terminalClosed ? 'hidden' : ''}`}>
+                <div className={`app flex flex-col bg-[#282a35] w-full h-full p-2 overflow-auto text-white relative ${terminalClosed ? 'hidden' : ''} ${terminalMinimized ? 'minimized' : ''}`}>
                     {showWelcome && <Welcome />}
                     <div className="terminal-info absolute bottom-0 left-0 w-full bg-[#282a35] text-white py-1 px-2 flex">
                         <span className="terminal-prefix text-green-400">user@terminal:~$</span>
