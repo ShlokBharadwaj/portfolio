728cb317b0453141c7e786b2f16a83572a593c6f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-06-26 13:23:13 +0530

    Add: Terminal closes when red button is clicked

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -10,6 +10,7 @@ import Welcome from './commands/Welcome';
 const Terminal = () => {
 
     const [showWelcome, setShowWelcome] = useState(false);
+    const [terminalClosed, setTerminalClosed] = useState(false);
 
     useEffect(() => {
         const delayToShowWelcome = async () => {
@@ -19,28 +20,32 @@ const Terminal = () => {
         delayToShowWelcome();
     }, []);
 
+    const closeTerminal = () => {
+        setTerminalClosed(true);
+    };
+
     return (
-        <div className='m-0 p-0 box-border h-[100vh] flex items-center justify-center bg-[#264653]'>
-            <div className='container flex flex-col w-full max-w-[800px] h-[500px] overflow-hidden rounded-lg m-5 cursor-text'>
+        <div className={`m-0 p-0 box-border h-[100vh] flex items-center justify-center bg-[#264653] ${terminalClosed ? 'animate-close' : ''}`}>
+            <div className="container flex flex-col w-full max-w-[800px] h-[500px] overflow-hidden rounded-lg m-5 cursor-text">
                 <div className="menu flex flex-row items-center text-center w-full h-6 bg-[#424040] py-0 px-2 cursor-default">
-                    <div className="buttons-flex absolute flex flex-row ">
-                        <div className="button red bg-[#f5544d] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
+                    <div className="buttons-flex absolute flex flex-row">
+                        <div className="button red bg-[#f5544d] w-3 h-3 rounded-full mr-2 cursor-pointer" onClick={closeTerminal}></div>
                         <div className="button yellow bg-[#fabd2f] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
                         <div className="button green bg-[#47d043] w-3 h-3 rounded-full mr-2 cursor-pointer"></div>
                     </div>
                     <div className="title flex-1 text-center text-slate-200">
                         <a href="https://github.com/ShlokBharadwaj/portfolio" target="_blank">
-                            <h1 className='hidden sm:block font-bold text-md'>
+                            <h1 className="hidden sm:block font-bold text-md">
                                 <FontAwesomeIcon icon={faGithub} className="pr-2" />
                                 github.com/ShlokBharadwaj
                             </h1>
                         </a>
-                        <h1 className='sm:hidden font-bold text-md'>
+                        <h1 className="sm:hidden font-bold text-md">
                             <FontAwesomeIcon icon={faGithub} className="pr-2 pl-11" />github.com/ShlokBharadwaj
                         </h1>
                     </div>
                 </div>
-                <div className="app flex flex-col bg-[#282a35] w-full h-full p-2 overflow-auto text-white relative">
+                <div className={`app flex flex-col bg-[#282a35] w-full h-full p-2 overflow-auto text-white relative ${terminalClosed ? 'hidden' : ''}`}>
                     {showWelcome && <Welcome />}
                     <div className="terminal-info absolute bottom-0 left-0 w-full bg-[#282a35] text-white py-1 px-2 flex">
                         <span className="terminal-prefix text-green-400">user@terminal:~$</span>
@@ -49,7 +54,7 @@ const Terminal = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Terminal
