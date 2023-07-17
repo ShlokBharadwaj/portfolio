cd45e946e5227fe945d7d370514fdaba51649fe0
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-17 20:29:36 +0530

    Update: About page's information again

--- src/components/commands/About.jsx
+++ src/components/commands/About.jsx
@@ -3,7 +3,7 @@ const About =  () => {
         <div className="text-white block">
             <p>Hi, My name is <span className="text-green-400 font-semibold">Shlok Bharadwaj.</span></p>
             <p>I'm a <span className="font-semibold">Full Stack Developer</span> from <span className="font-semibold">India.</span></p>
-            <p>I'm passionate about solving real-life problems and developing Android & Web Applications.</p>
+            <p>I'm passionate about solving real-life problems and developing <span className="font-semibold">Android</span> & <span className="font-semibold">Web Applications.</span></p>
         </div>
     );
 };
