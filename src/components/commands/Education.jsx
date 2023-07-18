8ddfa18604c86d40d943ae4eb10d9ee3f625e7d2
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-18 17:16:33 +0530

    Add: My Educational background

--- src/components/commands/Education.jsx
+++ src/components/commands/Education.jsx
@@ -1,7 +1,23 @@
-const Education =  () => {
-    return(
-        <div>
-            <h1>This is the Education page.</h1>
+import React from 'react';
+
+const Education = () => {
+    return (
+        <div className="text-white block">
+            <p className="mb-4">My Academic Journey:</p>
+            <ul className="list-disc pl-6">
+                <li>
+                    <span className="font-semibold">Bachelor of Technology in Computer Science</span><br />
+                    MATS University | CGPA: 9.54 | 2019-2023
+                </li>
+                <li>
+                    <span className="font-semibold">Class 12th</span><br />
+                    Baldwin Farm Area High School | Percentage: 59.20 | 2016 - 2019
+                </li>
+                <li>
+                    <span className="font-semibold">Class 10th</span><br />
+                    Baldwin Farm Area High School | CGPA: 9.2 | 2016
+                </li>
+            </ul>
         </div>
     );
 };
